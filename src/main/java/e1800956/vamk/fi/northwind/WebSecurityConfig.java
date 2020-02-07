d586a6e7f0ac465d998759c16b2c881d9b5afd97
Tao la` Minh <47974381+anhminh10a2hoa@users.noreply.github.com>
2020-02-07 11:36:32 +0200

    keystore and https

---
+++ src/main/java/e1800956/vamk/fi/northwind/WebSecurityConfig.java
@@ -0,0 +1,50 @@
+package e1800956.vamk.fi.northwind;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.anyRequest().fullyAuthenticated()
+				.and()
+			.formLogin();
+	}
+
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.ldapAuthentication()
+		        .userSearchFilter("uid={0}")
+		        .contextSource()
+		        .url("ldap://ldap.puv.fi:389/dc=puv,dc=fi");
+	}
+	
+	/*old version using application.properties and spring.ldap.embedded.ldif=classpath:test-server.ldif
+	 	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.ldapAuthentication()
+				.userDnPatterns("uid={0},ou=people")
+				.groupSearchBase("ou=groups")
+				.contextSource(contextSource())
+				.passwordCompare()
+					.passwordEncoder(new LdapShaPasswordEncoder())
+					.passwordAttribute("userPassword");
+	}
+	@Bean
+	public DefaultSpringSecurityContextSource contextSource() {
+		return  new DefaultSpringSecurityContextSource(Arrays.asList("ldap://localhost:8389/"), "dc=springframework,dc=org");
+	}
+	 */
+}
